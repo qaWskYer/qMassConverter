4b25869015cea9c149fddb3e27471303d7a9f95a
qaWskYer <qawskyer@gmail.com>
2025-04-01 11:20:18 +0300

    tests

--- test/MassConverter/MassConvertTest.php
+++ test/MassConverter/MassConvertTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace MassConverter\Test;
 
+use MassConverter\Converter\JpegToGifConverter;
 use MassConverter\Converter\JpegToPngConverter;
 use MassConverter\FileFinder\FileFinder;
 use MassConverter\FileFinder\RegexFilter;
@@ -38,5 +39,21 @@ class MassConvertTest extends TestCase
         $this->assertEquals([], $converter->getErrors());
         $this->assertTrue($result);
         $this->assertFileExists($dirDestination . 'SampleJPGImage_50kbmb.png');
+    }
+    
+    public function test_convertToGif()
+    {
+        $reflClass = new \ReflectionClass(get_class($this));
+        $dirSource      = dirname($reflClass->getFileName()) . DIRECTORY_SEPARATOR . '_data' . DIRECTORY_SEPARATOR . 'test1' . DIRECTORY_SEPARATOR;
+        $dirDestination = dirname($reflClass->getFileName()) . DIRECTORY_SEPARATOR . '_data' . DIRECTORY_SEPARATOR . 'test2_dest' . DIRECTORY_SEPARATOR;
+        
+        $converter = new MassConverter($dirSource, new FileFinder(new JpegValidator(), new RegexFilter('/.*/')), new JpegToGifConverter());
+        $result = $converter->collectFiles()
+            ->enableCreateDirectory(true)
+            ->convert($dirDestination);
+        
+        $this->assertEquals([], $converter->getErrors());
+        $this->assertTrue($result);
+        $this->assertFileExists($dirDestination . 'SampleJPGImage_50kbmb.gif');
     }
 }
